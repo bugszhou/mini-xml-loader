2707fe4193265cc344c3fcbe935e4da36d715b25
bugszhou <bugszhou@outlook.com>
2019-04-04 18:12:47 +0800

    fix(index): 获取xml里面的图片链接错误

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const path = require('path'),
   loaderUtils = require("loader-utils");
 
 function miniXmlLoader(source) {
-  const importReg = /<[(import|include|wxs|import\-sjs)][\s\S]*?src=[\"|\']([^\"]*?)[\"|\'][\s\S]*?>/gi,
+  const importReg = /<[(import|include|wxs|import\-sjs)][\s\S]*?src=[\"|\']([^\"]*)[\"|\'][\s\S]*?>/gi,
     varInputReg = /\{\{[\s\S]*?\}\}/gi;
   let result = [],
     importArr = [];
